15e6f0f2ee43e8f9580c50c20e8bd753bf4baf9f
ozdalkiran-l <yacob.ozdalkiran@gmail.com>
2026-01-09 16:00:20 +0100

    Added ecmc_frozen, geometryFrozen, mean_plaquette_frozen

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include "src/ecmc/ecmc.h"
+#include "src/ecmc/ecmc_frozen.h"
 #include <chrono>
 
 int main(int argc, char* argv[]) {
     int L = atoi(argv[1]);
     int T = atoi(argv[1]);
     GaugeField field(L, T);
-    Geometry geo(L, T);
+    GeometryFrozen geo(L, T);
     std::random_device rd;
     std::mt19937_64 rng(rd());
     auto start = std::chrono::high_resolution_clock::now();
-    ecmc::samples_improved(field, geo, atoi(argv[2]), atoi(argv[3]), atoi(argv[4]),atoi(argv[5]), atoi(argv[6]),atoi(argv[7]),rng);
+    ecmc_frozen::samples_improved(field, geo, atoi(argv[2]), atoi(argv[3]), atoi(argv[4]),atoi(argv[5]), atoi(argv[6]),atoi(argv[7]),rng);
     auto end = std::chrono::high_resolution_clock::now();
     auto elapsed = end - start;
     std::cout << "Elapsed time : " << std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count() << " ms" << std::endl;
