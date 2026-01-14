dd4a0e0c8ac0fdf14f1fa0d10f701c727938a53a
ozdalkiran-l <yacob.ozdalkiran@gmail.com>
2026-01-14 21:09:16 +0100

    New inputs for beta benchmark

---
+++ run_beta.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --job-name=beta_array
+#SBATCH --output=%x_%a_%A.o
+#SBATCH --time=00:50:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=16
+#SBATCH --array=1-9
+#SBATCH --partition=cpu_short
+
+# Run MPI script
+srun build/gauge_ecmc_mpi inputs/beta${SLURM_ARRAY_TASK_ID}.txt
