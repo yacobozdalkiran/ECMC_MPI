dab710d497378f92eead692369123a11f22dc7d8
ozdalkiran-l <yacob.ozdalkiran@gmail.com>
2026-01-14 21:17:50 +0100

    Change partition

--- run_beta.sh
+++ run_beta.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=16
 #SBATCH --array=1-9
-#SBATCH --partition=cpu_short
+#SBATCH --partition=cpu_med
 
 # Run MPI script
 srun build/gauge_ecmc_mpi inputs/beta${SLURM_ARRAY_TASK_ID}.txt
