8e40bea9c2eebeb9c814c3af8853d560f53b7da2
ozdalkiran-l <yacob.ozdalkiran@gmail.com>
2026-01-14 18:25:07 +0100

    Add generate_fullshift

--- src/bin/gauge_ecmc_mpi.cpp
+++ src/bin/gauge_ecmc_mpi.cpp
@@ -61,6 +61,7 @@ void generate(const RunParams &run_params) {
         std::cout << "Beta : " << ecmc_params.beta << "\n";
         std::cout << "Total number of shifts : " << n_shift*4 << "\n";
         std::cout << "Number of samples per shift : " << ecmc_params.N_samples << "\n";
+        std::cout << "Total number of samples : " << ecmc_params.N_samples*n_shift*4 << "\n";
         std::cout << "==========================================" << std::endl;
     }
 
@@ -84,6 +85,94 @@ void generate(const RunParams &run_params) {
         sp.coord=T;
         mpi::shift::shift(field, geo, halo_shift, topo, sp);
         plaquette[gshiftc*4+3] = mpi::ecmc::samples_improved(field, geo, ecmc_params, rng, halo_obs, topo);
+    }
+
+    if (topo.rank == 0) {
+        //Flatten the plaquette vector
+        std::vector<double> plaquette_flat(total_shifts*N_samples);
+        for (int i = 0; i<total_shifts; i++) {
+            for (int j = 0; j<N_samples; j++) {
+                plaquette_flat[i*N_samples + j] = plaquette[i][j];
+            }
+        }
+        //Write the output
+        std::string filename = "L"+std::to_string(L*n_core_dims)
+                                + "b"+std::to_string(ecmc_params.beta)
+                                + "Ls" + std::to_string(run_params.L_shift)
+                                + "Ns" + std::to_string(run_params.n_shift)
+                                + "dir" + std::to_string(run_params.stype_pos);
+        int precision = 10;
+        io::save_double_params(plaquette_flat, run_params, filename, precision);
+    }
+}
+
+void generate_fullshift(const RunParams &run_params) {
+    int L = run_params.L_core;
+    int n_core_dims = run_params.n_core_dims;
+
+    //Create the topology
+    mpi::MpiTopology topo(n_core_dims);
+
+    //Rng
+    std::random_device rd;
+    std::mt19937_64 rng(rd());
+
+    //Create the geometry and the field (cold)
+    mpi::GeometryFrozen geo(L);
+    GaugeField field(geo);
+
+    //Initialize the field
+    if (!run_params.cold_start) field.hot_start(rng);
+
+    //Create the observables halos
+    HaloObs halo_obs(geo);
+
+    //Shift params and halo creation
+    int L_shift = run_params.L_shift; //Max shift length
+    Halo halo_shift(L_shift, geo);
+    ShiftParams sp; //pos and coord and L_shift unset by default
+    if (run_params.stype_pos) sp.stype = pos;
+    else sp.stype = neg;
+    sp.L_shift = L_shift;
+
+    //Global shift parameters
+    int n_shift = run_params.n_shift;
+    int total_shifts = n_shift*4;
+
+    //ECMC params
+    ECMCParams ecmc_params = run_params.ecmc_params;
+    int N_samples = ecmc_params.N_samples;
+
+    //Space reserved for results
+    std::vector<std::vector<double>> plaquette(total_shifts);
+
+    //Print parameters
+    if (topo.rank == 0) {
+        std::cout << "==========================================" << std::endl;
+        std::cout << "Total lattice size : " << L*n_core_dims  << "\n";
+        std::cout << "Local lattice size : " << L << "\n";
+        std::cout << "Beta : " << ecmc_params.beta << "\n";
+        std::cout << "Total number of shifts : " << n_shift*4 << "\n";
+        std::cout << "Number of samples per shift : " << ecmc_params.N_samples << "\n";
+        std::cout << "Total number of samples : " << ecmc_params.N_samples*n_shift*4 << "\n";
+        std::cout << "==========================================" << std::endl;
+    }
+
+    for (int gshiftc = 0; gshiftc<total_shifts; gshiftc++) {
+        if (topo.rank == 0) std::cout << "Shift " << gshiftc << " (X)...\n";
+        sp.coord=X;
+        mpi::shift::shift(field, geo, halo_shift, topo, sp);
+        if (topo.rank == 0) std::cout << "Shift " << gshiftc << " (Y)...\n";
+        sp.coord=Y;
+        mpi::shift::shift(field, geo, halo_shift, topo, sp);
+        if (topo.rank == 0) std::cout << "Shift " << gshiftc << " (Z)...\n";
+        sp.coord=Z;
+        mpi::shift::shift(field, geo, halo_shift, topo, sp);
+        if (topo.rank == 0) std::cout << "Shift " << gshiftc << " (T)...\n";
+        sp.coord=T;
+        mpi::shift::shift(field, geo, halo_shift, topo, sp);
+
+        plaquette[gshiftc] = mpi::ecmc::samples_improved(field, geo, ecmc_params, rng, halo_obs, topo);
     }
 
     if (topo.rank == 0) {
@@ -140,7 +229,7 @@ int main(int argc, char* argv[]) {
     MPI_Barrier(MPI_COMM_WORLD);
     double start_time = MPI_Wtime();
 
-    generate(params);
+    generate_fullshift(params);
 
     MPI_Barrier(MPI_COMM_WORLD);
     double end_time = MPI_Wtime();
