6ed8914a486b4d66ea8134a2dbe2feadef75a21b
ozdalkiran-l <yacob.ozdalkiran@gmail.com>
2026-01-14 17:02:36 +0100

    Test compilation intel

--- modules_load.sh
+++ modules_load.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 module purge
-module load gcc/13.2.0/gcc-4.8.5
-module load intel-oneapi-mpi/2021.10.0/gcc-13.2.0
-module load cmake/3.21.4/gcc-13.2.0
+module load intel-oneapi-compilers/2023.2.1/gcc-11.2.0
+module load intel-oneapi-mpi/2021.10.0/oneapi-2023.2.1
+module load cmake/3.28.3/gcc-11.2.0
