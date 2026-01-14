f679c5d464587107a28b1bba93bf903d706a5537
ozdalkiran-l <yacob.ozdalkiran@gmail.com>
2026-01-14 16:17:50 +0100

    Minor change module_load

--- modules_load.sh
+++ modules_load.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 module load gcc/13.2.0/gcc-4.8.5
-module load openmpi/4.1.5/gcc-13.2.0
+# Charger les compilateurs OneAPI permet souvent d'exposer TBB
+module load intel-oneapi-compilers/2023.2.1/gcc-11.2.0
+module load intel-oneapi-mpi/2021.10.0/gcc-13.2.0
 module load cmake/3.21.4/gcc-13.2.0
