21d7c93044b3cfefb23f23ccc0b282338550744f
ozdalkiran-l <yacob.ozdalkiran@gmail.com>
2026-01-28 15:55:35 +0100

    Scripts benchmark hb mpi

---
+++ hbcold30k.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=hbcold30k
+#SBATCH --output=%x.o
+#SBATCH --time=02:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=16
+#SBATCH --ntasks-per-node=16
+#SBATCH --partition=cpu_med
+
+#Source necessary modules
+source modules_load.sh
+
+# Run MPI script
+srun build/gauge_ecmc_mpi inputs/heatbath_mpi_benchmark/cold30k.txt
