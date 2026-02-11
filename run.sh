0c00932d70ec7c7dec1d0d24a1dfe19401f4334b
ozdalkiran-l <yacob.ozdalkiran@gmail.com>
2026-02-11 15:07:50 +0100

    Input ecmc sc

--- run.sh
+++ run.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-#SBATCH --job-name=hot30k
+#SBATCH --job-name=ecmc_sc
 #SBATCH --output=%x.o
 #SBATCH --time=02:00:00
-#SBATCH --nodes=1
-#SBATCH --ntasks=16
-#SBATCH --ntasks-per-node=16
+#SBATCH --ntasks=1
 #SBATCH --partition=cpu_med
 
 #Source necessary modules
 source modules_load.sh
 
 # Run MPI script
-srun build/gauge_ecmc_mpi inputs/hot30k.txt
+srun --unbuffered build/gauge_single_core inputs/testsc.txt
